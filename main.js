3fe8f2d1e8850d4a0ecd17a7a3ad2f5e286bf9e3
Артем Баторшин <norvellart@gmail.com>
2021-09-16 17:40:51 +0300

    push to repository

---
+++ main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('#cage').bind('click', function(){
+        $('.main img').attr('src', 'icons_practice_module_3/cat_cage_256.png')
+    })
+    $('#box').bind('click', function(){
+        $('.main img').attr('src', 'icons_practice_module_3/cat_box_256.png')
+    })
+    $('#clean').bind('click', function(){
+        $('.main img').attr('src', 'icons_practice_module_3/cat_clean_256.png')
+    })
+    $('#eyes').bind('click', function(){
+        $('.main img').attr('src', 'icons_practice_module_3/cat_eyes_256.png')
+    })
+    $('#purr').bind('click', function(){
+        $('.main img').attr('src', 'icons_practice_module_3/cat_purr_256.png')
+    })
+    $('#tied').bind('click', function(){
+        $('.main img').attr('src', 'icons_practice_module_3/cat_tied_256.png')
+    })
+});
